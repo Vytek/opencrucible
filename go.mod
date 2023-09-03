256970db3309f294a850c2dff5368f7a0de4e2e2
Enrico Speranza <enrico.speranza@gmail.com>
2023-09-03 17:06:23 +0000

    Add TXT and RTF Parser

--- go.mod
+++ go.mod
@@ -4,7 +4,10 @@ go 1.21.0
 
 require github.com/gabriel-vasile/mimetype v1.4.2
 
+require github.com/EndFirstCorp/peekingReader v0.0.0-20171012052444-257fb6f1a1a6 // indirect
+
 require (
 	github.com/h2non/filetype v1.1.3
+	github.com/lu4p/cat v0.1.5
 	golang.org/x/net v0.8.0 // indirect
 )
--- opencrucible.go
+++ opencrucible.go
@@ -6,22 +6,48 @@ import (
 
 	"github.com/gabriel-vasile/mimetype"
 	"github.com/h2non/filetype"
+	"github.com/lu4p/cat"
 )
 
-func DetectFileTypeMIME(FileToDetect []byte) (string, string, error) {
-	// If no FileToDetect was given, return an error with a message.
-	if len(FileToDetect) == 0 {
-		return "", "", errors.New("file to detect is empty")
+// Version exposes the current package version.
+const Version = "0.0.1"
+
+//Detects
+
+func DetectFileTypeMIME(StreamToDetect []byte) (string, string, error) {
+	// If no StreamToDetect was given, return an error with a message.
+	if len(StreamToDetect) == 0 {
+		return "", "", errors.New("stream to parse is empty")
 	}
-	mtype, err := mimetype.DetectReader(bytes.NewReader(FileToDetect))
+	mtype, err := mimetype.DetectReader(bytes.NewReader(StreamToDetect))
 	return mtype.String(), mtype.Extension(), err
 }
 
-func DetectFileType(FileToDetect []byte) (string, string, error) {
-	kind, _ := filetype.Match(FileToDetect)
+func DetectFileType(StreamToDetect []byte) (string, string, error) {
+	kind, _ := filetype.Match(StreamToDetect)
 	if kind == filetype.Unknown {
 		return "", "", errors.New("unknown file type")
 	} else {
 		return kind.MIME.Value, kind.Extension, nil
 	}
+}
+
+//Parsers
+
+func TXTParseToString(StreamToParse []byte) (string, error) {
+	// If no StreamToDetect was given, return an error with a message.
+	if len(StreamToParse) == 0 {
+		return "", errors.New("stream to parse is empty")
+	}
+	txt, err := cat.FromBytes(StreamToParse)
+	return txt, err
+}
+
+func RTFParseToString(StreamToParse []byte) (string, error) {
+	// If no StreamToDetect was given, return an error with a message.
+	if len(StreamToParse) == 0 {
+		return "", errors.New("stream to parse is empty")
+	}
+	rtf, err := cat.FromBytes(StreamToParse)
+	return rtf, err
 }

d424a3bb006be1f0ea84875823b46d889326e1d7
Enrico Speranza <enrico.speranza@gmail.com>
2023-09-04 13:29:06 +0000

    Start using fileconversion for docx 2 txt

--- opencrucible.go
+++ opencrucible.go
@@ -3,6 +3,8 @@ package opencrucible
 import (
 	"bytes"
 	"errors"
+	"fmt"
+	"os"
 
 	"github.com/gabriel-vasile/mimetype"
 	"github.com/h2non/filetype"
@@ -52,13 +54,15 @@ func RTFParseToString(StreamToParse []byte) (string, error) {
 	return rtf, err
 }
 
-func DOCXParseToString(StreamToParse []byte) (string, error) {
-	// If no StreamToDetect was given, return an error with a message.
-	if len(StreamToParse) == 0 {
-		return "", errors.New("stream to parse is empty")
+func DOCXFileParseToString(FileToParse string) (string, error) {
+	// extract text from an XLSX file
+	file, err := os.Open(FileToParse)
+	if err != nil {
+		return "", errors.New(fmt.Sprintf("Error opening file: %s\n", err))
 	}
-	docx, err := cat.FromBytes(StreamToParse)
-	return docx, err
+
+	defer file.Close()
+	stat, _ := file.Stat()
 }
 
 func ODTParseToString(StreamToParse []byte) (string, error) {
